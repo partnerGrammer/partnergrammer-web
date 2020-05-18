efe9fc5181b109d44ac9a4fca1af2327218ad420
Horacio Adame <horacio@partnergrammer.com>
2020-05-18 09:22:14 -0600

    titulos de cada pagina

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -7,9 +7,18 @@ const router = new VueRouter({
     mode: 'hash',
     routes: [
         {
+//           Inicio
+// Sobre-nosotros
+// Nuestro-portafolio
+// Programa-de-alianza
+// Contacto
+
             path: '/',
             name: 'Index',
             component: require('./pages/Home').default,
+            meta: {
+              title: 'Inicio'
+            }
             // component: () => import('./pages/Home.vue')
         },
 
@@ -17,6 +26,9 @@ const router = new VueRouter({
             path: '/projects',
             name: 'Projects',
             component: require('./pages/Projects').default,
+            meta: {
+              title: 'Nuestro Portafolio'
+            }
             // component: () => import('./pages/Projects.vue')
         },
 
@@ -25,6 +37,9 @@ const router = new VueRouter({
             props: true,
             name: 'Project',
             component: require('./pages/Project').default,
+            meta: {
+              title: 'Nuestro Portafolio'
+            }
             // component: () => import('./pages/Project.vue')
         },
 
@@ -32,6 +47,9 @@ const router = new VueRouter({
             path: '/blog',
             name: 'Blog',
             component: require('./pages/Blog').default,
+            meta: {
+              title: 'Blog'
+            }
             // component: () => import('./pages/Blog.vue')
         },
 
@@ -40,6 +58,9 @@ const router = new VueRouter({
             props: true,
             name: 'Article',
             component: require('./pages/Article').default,
+            meta: {
+              title: 'Blog'
+            }
             // component: () => import('./pages/Article.vue')
         },
 
@@ -47,6 +68,9 @@ const router = new VueRouter({
             path: '/about',
             name: 'About',
             component: require('./pages/About').default,
+            meta: {
+              title: 'Sobre Nosotros'
+            }
             // component: () => import('./pages/About.vue')
         },
 
@@ -60,9 +84,22 @@ const router = new VueRouter({
             path: '*',
             name: '404',
             component: require('./pages/404').default,
+            meta: {
+              title: '404'
+            }
             // component: () => import('./pages/404.vue')
         }
     ],
 });
+
+const DEFAULT_TITLE = 'Some Default Title';
+router.afterEach((to, from) => {
+    // Use next tick to handle router history correctly
+    // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+    Vue.nextTick(() => {
+        document.title = to.meta.title || DEFAULT_TITLE;
+    });
+});
+
 
 export default router;
