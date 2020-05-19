75b44c4117970e5cde6d5f7166e56564966d4d39
Horacio Adame <horacio@partnergrammer.com>
2020-05-19 15:31:59 -0600

    SEO numero de telefono y direccion

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -7,26 +7,42 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description"
-    content="Partnergrammer ayuda a marcas globales a diseñar, construir y lanzar productos digitales. Desarrollamos software a la medida, sistemas web, móviles o de escritorio con soluciones innovadoras. Pide tu cotización ahora.">
+    content="Partnergrammer ayuda a marcas globales a diseñar, construir y lanzar productos digitales. Desarrollamos software a la medida, sistemas web, móviles o de escritorio con soluciones innovadoras. Pide tu cotización ahora al (614) 303-8662, o visitanos en Av. Heroico Colegio Militar 3700, Bodegas del Estado, 31300 Chihuahua, Chih..">
 
     <meta name="keywords"
-    content="desarrollo web con python, desarrollo web con php y mysql, desarrollo web angular, desarrollo a la medida, desarrollo web agencia, desarrollo web aplicaciones móviles y big data, desarrollo web aguascalientes, desarrollo web a medida, desarrollo web backend, desarrollo web bootstrap, desarrollo web backend y frontend, desarrollo web en mexico, desarrollo web empresas, desarrollo de aplicaciones web, desarrollo web e-commerce, desarrollo web freelance, desarrollo web guadalajara, ,desarrollo web  html, desarrollo web html y css, desarrollo web html5,  desarrollo web java, desarrollo web jquery, desarrollo web javascript
-,software ala medida
-,cuanto cuesta un software a la medida
-,que es software a la medida
-,para que sirve software a medida
-,que significa software a medida
-,software a la medida mexico
-,software a la medida de su empresa
-,desarrollo software a la medida
-,desarrollo de software a la medida mexico
-,software a la medida en monterrey
-,software a la medida en juarez
-,software a medida para empresas
-,software a medida y soluciones empaquetadas
-,diseño y construcción de software a la medida para el sector empresarial
-,cuanto cuesta un software a la medida
-">
+    content="desarrollo web con python,
+    desarrollo web con php y mysql,
+    desarrollo web angular, desarrollo a la medida,
+    desarrollo web agencia, desarrollo web aplicaciones móviles y big data,
+    desarrollo web aguascalientes, desarrollo web a medida,
+    desarrollo web backend, desarrollo web bootstrap,
+     desarrollo web backend y frontend, desarrollo web en mexico,
+     desarrollo web empresas, desarrollo de aplicaciones web, desarrollo web e-commerce,
+     desarrollo web freelance, desarrollo web guadalajara, ,desarrollo web  html,
+     desarrollo web html y css, desarrollo web html5,  desarrollo web java,
+     desarrollo web jquery, desarrollo web javascript
+    ,software ala medida
+    ,cuanto cuesta un software a la medida
+    ,que es software a la medida
+    ,para que sirve software a medida
+    ,que significa software a medida
+    ,software a la medida mexico
+    ,software a la medida de su empresa
+    ,desarrollo software a la medida
+    ,desarrollo de software a la medida mexico
+    ,software a la medida en monterrey
+    ,software a la medida en juarez
+    ,software a medida para empresas
+    ,software a medida y soluciones empaquetadas
+    ,diseño y construcción de software a la medida para el sector empresarial
+    ,cuanto cuesta un software a la medida">
+
+    <meta name="og:phone_number" content="(614) 303-8662"/>
+    <meta name="og:street-address" content="Av. Heroico Colegio Militar 3700"/>
+    <meta name="og:locality" content="Chihuahua"/>
+    <meta name="og:region" content="Chih"/>
+    <meta name="og:postal-code" content="31300"/>
+    <meta name="og:country-name" content="MEX"/>
 
     <title>{{ config('app.name', 'PartnerGrammer') }}</title>
 
